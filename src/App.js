acf197025fe0e60d443b7b30ecf28fc3a3eda34b
John Malapit <johncmalapit@gmail.com>
2023-02-06 11:27:17 -0500

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {}, []);
   return (
     <div className='App'>
       <h1>Hackflix</h1>
